d949dc4d492e33009e9ecb2ced5559e641249272
Jan Cimbalnik <jan.cimbalnik@gmail.com>
2016-06-16 15:20:46 +0200

    Added io function for automated dataframe creation.

--- pyhfo_detect/io/data_operations.py
+++ pyhfo_detect/io/data_operations.py
@@ -4,6 +4,8 @@
 
 from . import D_file_read
 import pyedflib
+import pandas as pd
+import numpy as np
 
 
 def data_feeder(file_path, start_samp, stop_samp, channel_name):
@@ -12,10 +14,10 @@ def data_feeder(file_path, start_samp, stop_samp, channel_name):
 
     Parameters:
     -----------
-    file_path - path to data file\n
-    start_samp - start sample\n
-    stop_samp - stop sample\n
-    channel_name - requested channel\n
+    file_path(str) - path to data file\n
+    start_samp(int) - start sample\n
+    stop_samp(int) - stop sample\n
+    channel_name(str) - requested channel\n
 
     Returns:
     --------
@@ -41,3 +43,38 @@ def data_feeder(file_path, start_samp, stop_samp, channel_name):
         data = f.readSignal(ch_idx)
 
     return data, fs
+
+
+def create_output_df(fields=[],dtypes=None):
+    """
+    Function to create a custom pandas dataframe depending on the algorithm
+    needs. Fields: event_start,event_stop preset.
+    
+    Parameters:
+    -----------
+    fields(list) - additional fields for the dataframe\n
+    dtypes(dict) - dictionary with dtypes for specified fields (optional)\n
+    
+    Returns:
+    --------
+    dataframe - pandas dataframe for deteciton insertion\n
+    """
+    
+    # Preset dtypes
+    dtype_dict = {'event_start':np.int64, 'event_stop':np.int64} 
+                  
+    if dtypes is not None:
+        dtype_dict.update(dtypes)
+    
+    out_df = pd.DataFrame(columns=['event_start', 'event_stop']+fields)
+                                   
+    for col in out_df.columns:
+        if col in dtype_dict.keys():
+            out_df[col] = out_df[col].astype(dtype_dict[col])
+            
+def add_metadata():
+    """
+    Function to add metadata to the output dataframe
+    
+    """
+        
