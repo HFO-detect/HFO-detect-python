22aba844014db39748b35d2aa6fe8c1fe9fd11d3
Jan Cimbalnik <jan.cimbalnik@gmail.com>
2016-10-20 20:05:58 +0200

    Started coding III stage detector (Fedele et al.)

---
+++ pyhfo_detect/core/III_stage_detector.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 20:00:40 2016
+
+III - stage detector provided by Tomasso Fedele.
+Reprogrammed to python by Jan Cimbalnik..
+
+If you use this detector please cite!
+
+Ing.,Mgr. (MSc.) Jan Cimbálník
+Biomedical engineering
+International Clinical Research Center
+St. Anne's University Hospital in Brno
+Czech Republic
+&
+Mayo systems electrophysiology lab
+Mayo Clinic
+200 1st St SW
+Rochester, MN
+United States
+"""
+
+from .Morphology_detctor import *
+
+# %% Run Morphology detector (Stages I and II)
+
+# %% Run Stage III - cross channel validation
