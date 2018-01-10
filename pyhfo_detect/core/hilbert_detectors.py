32b89f46025ca296d6f4996117adafba4929dae0
Jan Cimbalnik <jan.cimbalnik@gmail.com>
2018-01-10 14:54:04 +0100

    Added multiprocessing and log spacing to hilbert detector

--- pyhfo_detect/core/hilbert_detectors.py
+++ pyhfo_detect/core/hilbert_detectors.py
@@ -19,6 +19,7 @@ United States
 """
 
 # Std imports
+from multiprocessing import Pool
 
 # Third pary imports
 import numpy as np
@@ -29,7 +30,8 @@ from ..io.data_operations import create_output_df, correct_boundary_dtype
 
 def hilbert_detector_1_0(data,fs,low_fc, high_fc, threshold = 3,
                          window = 10, window_overlap = 0,
-                         cyc_th = 1, gap_th = 1):
+                         band_spacing = 'linear', num_bands = 300, 
+                         cyc_th = 1, gap_th = 1, mp = 1):
 
     """
     Slightly modified algorithm which uses the 2D HFO hilbert detection.\n
@@ -50,9 +52,12 @@ def hilbert_detector_1_0(data,fs,low_fc, high_fc, threshold = 3,
         high_fc(float) - high cut-off frequency\n
         window(float) - statstical window in secs\n
         window_overlap(float) - stat. window overlap (range 0-1)\n
+        band_spacing(string) - options: 'linear', 'log' ('linear')\n
+        num_bands(int) - number of bands if band_spacing = log (300)\n
         threshold(float) - threshold for detection (3)\n
-        cyc_th - minimum number of cycles to detect (1)\n
-        gap_th - number of cycles for gaps (1)\n
+        cyc_th(float) - minimum number of cycles to detect (1)\n
+        gap_th(float) - number of cycles for gaps (1)\n
+        mp(int) - number of cores to use (def = 1)\n
 
     Returns:
     ---------
@@ -68,11 +73,26 @@ def hilbert_detector_1_0(data,fs,low_fc, high_fc, threshold = 3,
     win_start = 0
     win_size = int(window * fs)
     
-    freq_span = high_fc - low_fc
     
-    thresh_sig = np.zeros(int(win_size), dtype='int8')
 
     df_idx = 0
+    
+    
+
+    # Construct filter cut offs
+    
+    if band_spacing == 'log':
+        low_fc = float(low_fc)
+        high_fc = float(high_fc)
+        coffs = np.logspace(0,np.log10(high_fc),num_bands)
+        coffs = coffs[(coffs>low_fc) & (coffs<high_fc)]
+        freq_span = len(coffs) - 1
+    elif band_spacing == 'linear':
+        coffs = np.arange(low_fc, high_fc)
+        freq_span = (high_fc - low_fc) - 1
+
+    #Create a pool of workers
+    if mp > 1: work_pool = Pool(mp)
 
     #Start the looping
     while win_start+win_size <= len(data):
@@ -81,96 +101,44 @@ def hilbert_detector_1_0(data,fs,low_fc, high_fc, threshold = 3,
 
         x = data[win_start:win_end]
 
-        """
-        Not using now - have to figure out the notch filter
-        #Apply 60/50 Hz notch filter
-        b,a = butter (4,[59.0/(Fs/2),61.0/(Fs/2)],'bandstop')
-        x_notched = filtfilt(b,a,data)
-        """
+        tdetects_concat = []
+        if mp > 1:
+        
+            
+            # Run the filters in their threads and return the result
+            iter_mat =  [[x, fs, coffs[i], coffs[i+1],
+                          cyc_th, gap_th, threshold] for i in range(freq_span)]
+            tdetects_concat = work_pool.map(band_z_score_detect,iter_mat)
 
-        print('Creating matrix')
-
-        #Apply high-pass 1 Hz
-        b,a = butter (4,1.0/(fs/2),'highpass')
-        x_cond = filtfilt(b,a,x)
-
-        tdetects = []
-        for i in range(freq_span):
-            bot = float(i+low_fc)
-            top = float(i+low_fc+1)
-    
-            b,a = butter (3,bot/(fs/2),'highpass')
-            fx = filtfilt (b,a,x_cond)
-    
-            b,a = butter (3,top/(fs/2),'lowpass')
-            fx = filtfilt (b,a,fx)
-    
-            #Compute the z-scores
-    
-            ms = np.mean(fx)
-            sd = np.std(fx)
-            fx = [(x-ms)/sd for x in fx]
-    
-            hfx = np.abs(hilbert(fx))
-    
-             # Create dot product and threshold the signal
-            thresh_sig[:] = 0
-            thresh_sig[hfx>threshold] = 1
-
-            #Now get the lengths
-            idx = 0
-            th_idcs = np.where(thresh_sig == 1)[0]
-            gap_samp = round(gap_th * fs / bot)
-            while idx < len(th_idcs)-1:
-                if (th_idcs[idx+1] - th_idcs[idx]) == 1:
-                    start_idx = th_idcs[idx]
-                    while idx < len(th_idcs)-1:
-                        if (th_idcs[idx+1] - th_idcs[idx]) == 1:
-                            idx += 1  # Move to the end of the detection
-                            if idx == len(th_idcs)-1:
-                                stop_idx = th_idcs[idx]
-                                #Check for number of cycles
-                                dur = (stop_idx-start_idx) / fs
-                                cycs = bot * dur
-                                if cycs > cyc_th:
-                                    # Carry the amplitude and frequency info
-                                    tdetects.append([i,start_idx,stop_idx,
-                                                     max(hfx[start_idx:stop_idx])])
-                        else:  # Check for gap
-                            if (th_idcs[idx+1] - th_idcs[idx]) < gap_samp:
-                                idx +=1
-                            else:
-                                stop_idx = th_idcs[idx]
-                                #Check for number of cycles
-                                dur = (stop_idx-start_idx) / fs
-                                cycs = bot * dur
-                                if cycs > cyc_th:
-                                    tdetects.append([i,start_idx,stop_idx,
-                                                     max(hfx[start_idx:stop_idx])])
-                                idx += 1
-                                break
-                else:
-                    idx += 1
-    
+            work_pool.join
+        else:
+            for i in range(freq_span):
+                bot = coffs[i]
+                top = coffs[i+1]
+                
+                args = [x, fs, bot, top, cyc_th, gap_th, threshold]
+                
+                tdetects_concat.append(band_z_score_detect(args))
 
          # Process detects
-        detects = np.array(tdetects)
+        detects = np.array([det for band in tdetects_concat for det in band])
 
-        # This will run per one master branch - should be wrapped in a while loop
         outlines = []
         if len(detects):
             while sum(detects[:,0] != 0):
                 det_idx = np.where(detects[:,0] !=0)[0][0]
                 HFO_outline = []
-                outlines.append(np.array(run_detect_branch(detects,det_idx,HFO_outline)))
+                outlines.append(np.array(run_detect_branch(detects,
+                                                           det_idx,
+                                                           HFO_outline)))
 
         # Get the detections
         for ouline in outlines[:10]:
             start = min(ouline[:,1])
             stop = max(ouline[:,2])
-            freq_min = low_fc + int(ouline[0,0])  # mat_HFO.bbox[0] + low_mat_fc
-            freq_max = low_fc + int(ouline[-1,0])  # mat_HFO.bbox[2] + low_mat_fc
-            frequency_at_max = low_fc + int(ouline[np.argmax(ouline[:,3]),0])
+            freq_min = int(ouline[0,0])
+            freq_max =  int(ouline[-1,0])
+            frequency_at_max = int(ouline[np.argmax(ouline[:,3]),0])
             max_amplitude = max(ouline[:,3])
 
             event_start = int(start + win_start)
@@ -194,12 +162,82 @@ def hilbert_detector_1_0(data,fs,low_fc, high_fc, threshold = 3,
 #            plt.show(block=False)
 #            plt.waitforbuttonpress(1)
         
+    if mp > 1: work_pool.close()
+        
     correct_boundary_dtype(df_out)
     return df_out
 
 # =============================================================================
 # Subfunctions
 # =============================================================================
+
+def band_z_score_detect(args):
+    
+    x_cond = args[0]
+    fs = args[1]
+    bot = args[2]
+    top = args[3]
+    cyc_th = args[4]
+    gap_th = args[5]
+    threshold = args[6]
+    
+    tdetects = []
+    thresh_sig = np.zeros(len(x_cond), dtype='int8')
+    
+    b,a = butter (3,bot/(fs/2),'highpass')
+    fx = filtfilt (b,a,x_cond)
+
+    b,a = butter (3,top/(fs/2),'lowpass')
+    fx = filtfilt (b,a,fx)
+
+    #Compute the z-scores
+
+    ms = np.mean(fx)
+    sd = np.std(fx)
+    fx = [(x-ms)/sd for x in fx]
+
+    hfx = np.abs(hilbert(fx))
+
+     # Create dot product and threshold the signal
+    thresh_sig[:] = 0
+    thresh_sig[hfx>threshold] = 1
+
+    #Now get the lengths
+    idx = 0
+    th_idcs = np.where(thresh_sig == 1)[0]
+    gap_samp = round(gap_th * fs / bot)
+    while idx < len(th_idcs)-1:
+        if (th_idcs[idx+1] - th_idcs[idx]) == 1:
+            start_idx = th_idcs[idx]
+            while idx < len(th_idcs)-1:
+                if (th_idcs[idx+1] - th_idcs[idx]) == 1:
+                    idx += 1  # Move to the end of the detection
+                    if idx == len(th_idcs)-1:
+                        stop_idx = th_idcs[idx]
+                        #Check for number of cycles
+                        dur = (stop_idx-start_idx) / fs
+                        cycs = bot * dur
+                        if cycs > cyc_th:
+                            # Carry the amplitude and frequency info
+                            tdetects.append([bot,start_idx,stop_idx,
+                                             max(hfx[start_idx:stop_idx])])
+                else:  # Check for gap
+                    if (th_idcs[idx+1] - th_idcs[idx]) < gap_samp:
+                        idx +=1
+                    else:
+                        stop_idx = th_idcs[idx]
+                        #Check for number of cycles
+                        dur = (stop_idx-start_idx) / fs
+                        cycs = bot * dur
+                        if cycs > cyc_th:
+                            tdetects.append([bot,start_idx,stop_idx,
+                                             max(hfx[start_idx:stop_idx])])
+                        idx += 1
+                        break
+        else:
+            idx += 1
+            
+    return tdetects
 
 def detection_overlap_check(gs,dd):
     """
@@ -253,7 +291,9 @@ def run_detect_branch(detects,det_idx,HFO_outline):
         for next_det_idx in next_band_idcs[0]:
             #detection = detects[0]
             if detection_overlap_check([detects[det_idx,1],detects[det_idx,2]],
-                                                      [detects[next_det_idx,1],detects[next_det_idx,2]]):
+                                                      [detects[next_det_idx,1],
+                                                       detects[next_det_idx,
+                                                               2]]):
                 #Go up the tree
                 run_detect_branch(detects,next_det_idx,HFO_outline)
 
